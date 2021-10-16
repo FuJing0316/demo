5c65f229adb904ad405664b069d26efd5a0141d6
FuJing0316 <582473443@qq.com>
2021-10-16 20:00:50 +0800

    demo first commit

---
+++ src/main/java/utils/string/fillBlank.java
@@ -0,0 +1,13 @@
+package utils.string;
+
+/**
+ * @Author: fujing
+ * @Date: 2021/10/16
+ * @Description:
+ * @Version: 1.0
+ */
+public class fillBlank {
+    public static void main(String[] args) {
+
+    }
+}
