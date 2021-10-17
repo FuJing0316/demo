60230c94e5eaa78430a95aeee1ed34981d92650e
FuJing0316 <582473443@qq.com>
2021-10-17 18:17:29 +0800

    Enum 补充

--- src/main/java/demo/EventEnum.java
+++ src/main/java/demo/EventEnum.java
@@ -3,7 +3,9 @@ package demo;
 /**
  * @Author: fujing
  * @Date: 2021/10/17
- * @Description: 枚举类也是类，一样有属性、方法和构造方法。
+ * @Description: 1、枚举类也是类，一样有属性、方法和构造方法。
+ *               2、每个被枚举的成员，默认是public static finnal 的，可以通过枚举类名直接使用它们。
+ *               3、当你需要使用一组常量时，建议使用枚举类
  * @Version: 1.0
  */
 public enum EventEnum {
